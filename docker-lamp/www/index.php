5415a4f9831e864dd86092c195175d780dab226a
pedro532 <94136408+pedro532@users.noreply.github.com>
2021-11-16 20:53:27 +0100

    Add files via upload

---
+++ docker-lamp/www/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>127.0.0.1</title>
+        <meta charset="utf-8"> 
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php
+                echo "<h1>id de algo</h1>";
+                echo "<p>123234 - carlos</p>";
+                echo "<p>345643 - miguel</p>";
+                echo "<p>987234 - paco</p>";
+                echo "<p>129685- carola</p>";
+                echo "<p>871186 - pepe</p>";
+                echo "<p>871186 - jaime</p>";
+                echo "<p>871186 - sara</p>";
+                echo "<p>871186 - raül</p>";
+            ?>
+        </div>
+    </body>
+</html>
